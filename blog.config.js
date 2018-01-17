c8146ee93f2cdcb56cf4d89c73b534168e2abaf4
dodgydre <mail@ohgeorgie.com>
2018-01-17 17:27:32 +0100

    shifting to home

---
+++ blog.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Andreas Blog',
+  userId: 'dodgydre',
+  gistId: '91753e30d80647110e5531e129ce5c72',
+}
---
+++ src/components/Page.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ $route.params.page_id }}
+    <div v-if="data">
+      {{ data }}
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+const blogConfig = require('./blog.config.js')
+
+export default {
+  data () {
+    return {
+      data: null
+    }
+  },
+  mounted () {
+    window.axios.get('https://api.github.com/users/dodgydre/gists')
+      .then((data) => {
+        this.data = data
+      })
+  },
+  methods: {
+    fetchPage () {
+      let url = `http://api.github.com/gists/${blogConfig.gistId}`
+      let
+    }
+  }
+}
+</script>
